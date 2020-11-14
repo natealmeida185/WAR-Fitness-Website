b34e5d01357a6c261d717d55edcc808664a0bc30
Nathan Almeida <nalmeida345@g.rwu.edu>
2020-11-14 12:11:37 -0500

    commit to 'master'

--- war-fitness/src/comps/Profile.js
+++ war-fitness/src/comps/Profile.js
@@ -4,6 +4,7 @@ import Hero from './Hero';
 const Profile = () => {
     return (
         <section className="profile-section">
+            <Hero />
             <div className="profile-info">
                 <h1>User Profile</h1>
             </div>
