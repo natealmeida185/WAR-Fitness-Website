28385b2eb1bc370751a8a45942b3c99e94b1b9a9
Nathan Almeida <nalmeida345@g.rwu.edu>
2020-11-22 19:08:50 -0500

    commit to 'master'

--- war-fitness/src/comps/Profile.js
+++ war-fitness/src/comps/Profile.js
@@ -6,23 +6,27 @@ const items1 = [
     {
         id: 1,
         value: 'Wide Grip Pushups (Until Failure)',
-        image: 'images/workout1.jpg',
+        image: 'images/wide-grip.jpg',
     },
     {
         id: 2,
         value: 'Bent-Over Row (3 Sets of 12 Reps)',
+        image: 'images/bent-over.jpg',
     },
     {
         id: 3,
         value: 'Mid Table-Row (3 Sets of 12 Reps)',
+        image: 'images/table-row.jpg',
     },
     {
         id: 4,
         value: 'Sliding Floor Pull-Up (2 Sets of 12 Reps)',
+        image: 'images/floor-pull.jpg',
     },
     {
         id: 5,
         value: 'Superman (2 Sets of 15 Reps)',
+        image: 'images/superman.jpg',
     }
 ]
 
@@ -30,19 +34,22 @@ const items2 = [
     {
         id: 1,
         value: 'Pike Push-Up (3 Sets of 12 Reps)',
-        image: 'images/workout1.jpg',
+        image: 'images/pike-push-up.jpg',
     },
     {
         id: 2,
         value: 'Push-Up Hold (2 Sets of 1 Minute Holds)',
+        image: 'images/push-up-hold.jpg',
     },
     {
         id: 3,
         value: 'Forward Lean Push-Up (Until Failure)',
+        image: 'images/lean-forward.jpg',
     },
     {
         id: 4,
         value: 'Shoulder Taps (3 Sets of 25 Reps)',
+        image: 'images/shoulder-taps.jpg',
     }
 ]
 
@@ -50,23 +57,27 @@ const items3 = [
     {
         id: 1,
         value: 'Front Squats (3 Sets of 15 Reps)',
-        image: 'images/workout1.jpg',
+        image: 'images/front-squat.jpg',
     },
     {
         id: 2,
         value: 'Side Lunges (3 Sets of 12 Reps)',
+        image: 'images/side-lunges.jpg',
     },
     {
         id: 3,
-        value: 'Pendulum Lunges (3 Sets of 12 Reps)',
+        value: 'Front Lunges (3 Sets of 12 Reps)',
+        image: 'images/front-lunges.jpg',
     },
     {
         id: 4,
         value: 'Single Leg Romanian Dead-Lift (3 Sets of 10 Reps)',
+        image: 'images/romanian-deadlift.jpg',
     },
     {
         id: 5,
         value: 'Step-Ups (3 Sets of 10 Reps)',
+        image: 'images/step-ups.jpg',
     }
 ]
 
@@ -74,19 +85,22 @@ const items4 = [
     {
         id: 1,
         value: 'Tricep-Dips (3 Sets of 12 Reps)',
-        image: 'images/workout1.jpg',
+        image: 'images/tricep-dips.jpg',
     },
     {
         id: 2,
         value: 'Diamond/Close-Grip Push-Up (Until Failure)',
+        image: 'images/diamond-pushups.jpg',
     },
     {
         id: 3,
         value: 'Bicep Pull Hold (1 Minute)',
+        image: 'images/pull-hold.jpg',
     },
     {
         id: 4,
         value: 'Bicep Curls (3 Sets of 20)',
+        image: 'images/bicep-curl.jpg',
     }
 ]
 
@@ -94,23 +108,27 @@ const items5 = [
     {
         id: 1,
         value: 'Sit-Ups (Until failure)',
-        image: 'images/workout1.jpg',
+        image: 'images/sit-ups.jpg',
     },
     {
         id: 2,
         value: 'Bicycle Crunches (3 Sets of 20 Reps)',
+        image: 'images/bicycle-crunch.jpg',
     },
     {
         id: 3,
         value: 'Partial Crunches (3 Sets of 20 Reps)',
+        image: 'images/partial-crunch.jpg',
     },
     {
         id: 4,
         value: 'Russian Twist (2 Sets of 20 Reps)',
+        image: 'images/russian-twist.jpg',
     },
     {
         id: 5,
         value: 'Plank (One Minute)',
+        image: 'images/plank.jpg',
     }
 ]
 const Profile = () => {
